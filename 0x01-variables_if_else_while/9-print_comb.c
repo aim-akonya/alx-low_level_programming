2d6e99bdd22ed2e56fa6bf68cdae77f4cc15e2a1
aim-akonya <aim.akonya8@gmail.com>
2022-03-12 00:28:05 +0300

    reverse alphabet

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,13 @@
 int main(void)
 {
 int c;
-char delimiter[]=", ";
 for (c = 0; c <= 9; c++)
 {
 putchar(c + '0');
 if ( c != 9)
 {
-putchar(delimiter[0]);
-putchar(delimiter[1]);
+putchar(',');
+putchar('\t');
 }
 }
 putchar('\n');
