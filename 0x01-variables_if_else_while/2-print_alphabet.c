9d07d8f18190ab6b9f71183c6d06c8745c7da4b8
aim-akonya <aim.akonya8@gmail.com>
2022-03-11 23:33:29 +0300

    feat: updates

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: returns 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+return (0);
+}
