5abeb3087be0b556b32dbe2a6e6918e7c0e6fc27
aim-akonya <aim.akonya8@gmail.com>
2022-03-11 23:45:14 +0300

    alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: returns 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c == 'q' || c == 'e'){
+continue;
+}
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
