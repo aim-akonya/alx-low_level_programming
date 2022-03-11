ad0de18d2ba9f7ceb7a92f771ab32d2b764a511d
aim-akonya <aim.akonya8@gmail.com>
2022-03-11 23:49:58 +0300

    alphabets

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: returns 0
+*/
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+printf("%d", c);
+}
+printf("\n");
+return (0);
+}
