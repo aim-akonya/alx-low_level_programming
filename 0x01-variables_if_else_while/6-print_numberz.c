0d652532ea4c4c56602b8a1d08058f5c4d4a6b40
aim-akonya <aim.akonya8@gmail.com>
2022-03-11 23:56:37 +0300

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: returns 0
+*/
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
