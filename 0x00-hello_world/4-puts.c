926a5f1d7559683dd278daee11cf306a63302cce
aim-akonya <aim.akonya8@gmail.com>
2022-03-14 23:10:39 +0300

    feat: update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+* main - main function
+* Return: - returns 0
+*/
+int main(void)
+{
+char str[] = "Programming is like building a multilingual puzzle\n";
+puts(str);
+return (0);
+}
