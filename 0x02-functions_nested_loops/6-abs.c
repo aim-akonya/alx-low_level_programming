120786a20f07132e574ceb5346e7485bcff8243b
aim-akonya <aim.akonya8@gmail.com>
2022-03-15 23:56:00 +0300

    feat: updates

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check the code
+ * @n: number
+ * Return: 0
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+_putchar(-n + '0');
+}
+else
+{
+_putchar(n + '0');
+}
+return (0);
+}
