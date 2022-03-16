035f4a52ebf2d34fd1e72be4a380f07e9c917441
aim-akonya <aim.akonya8@gmail.com>
2022-03-16 22:45:26 +0300

    feat: table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Times table
+*/
+void times_table(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+int j;
+for (j = 0; j <= 9; j++)
+{
+
+int mult = i * j;
+if (mult <= 9)
+{
+_putchar(mult + '0');
+}
+else
+{
+_putchar(mult / 10 + '0');
+_putchar(mult % 10 + '0');
+}
+
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
