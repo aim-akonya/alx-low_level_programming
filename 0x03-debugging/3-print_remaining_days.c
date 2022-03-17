31410db45cb3e4fa5b7f7b8a140c8725dfc0f53f
aim-akonya <aim.akonya8@gmail.com>
2022-03-17 23:16:42 +0300

    updates

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,18 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+	int isLeap;
+
+	if (year % 400 == 0)
+		isLeap = 1;
+	else if (year % 100 == 0)
+		isLeap = 0;
+	else if (year % 4 == 0)
+		isLeap = 1;
+	else
+		isLeap = 0;
+
+	if (isLeap == 1)
 	{
 		if (month >= 2 && day >= 60)
 		{
