48e87559a8c4c15c7d43d677c8ef1b2708eedffe
michael <aim.akonya8@gmail.com>
2023-02-18 05:27:37 -0800

    fix: 4-print-alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,16 @@
 */
 int main(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-{
-if (c == 'q' || c == 'e')
-{
-continue;
-}
-putchar(c);
-}
-putchar('\n');
-return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
