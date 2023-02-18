877325a2239ecc8c592490db12ae7dc93cb48a34
michael <aim.akonya8@gmail.com>
2023-02-18 05:48:40 -0800

    fix: 9-print_comb fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,17 @@
 */
 int main(void)
 {
-int c;
-for (c = 0; c <= 9; c++)
-{
-putchar(c + '0');
-if (c != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '0');
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
