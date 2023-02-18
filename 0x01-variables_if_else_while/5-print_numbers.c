cc60d6017c144793253fcb76d43009ea70483806
michael <aim.akonya8@gmail.com>
2023-02-18 05:30:22 -0800

    print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,12 @@
 */
 int main(void)
 {
-int c;
-for (c = 0; c <= 9; c++)
-{
-printf("%d", c);
-}
-printf("\n");
-return (0);
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+	return (0);
 }
