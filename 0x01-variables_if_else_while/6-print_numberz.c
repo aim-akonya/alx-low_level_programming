12bb6e25d9d2c3f1150845f59f783f9893ffa02c
michael <aim.akonya8@gmail.com>
2023-02-18 05:33:16 -0800

    fx: upd

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,12 @@
 */
 int main(void)
 {
-int c;
-for (c = 0; c <= 9; c++)
-{
-putchar(c + '0');
-}
-putchar('\n');
-return (0);
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '\0');
+	}
+	putchar('\n');
+	return (0);
 }
