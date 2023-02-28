62b48b3a8c4544bd564793c863260d88f0c8a023
michael <aim.akonya8@gmail.com>
2023-02-28 04:52:07 -0800

    feat: updates

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,25 +9,25 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-        char buffer[2];
-        int value = 14;
-        
-        while (value / 10)
-        {
-            buffer[j] = value % 10 + '0';
-            value = value / 10;
-            j++;
-        }
+		char buffer[2];
+		int value = 14;
 
-        buffer[j] = value + '0';
+		while (value / 10)
+		{
+			buffer[j] = value % 10 + '0';
+			value = value / 10;
+			j++;
+		}
 
-        for (j = sizeof(buffer) - 1; j >= 0; j--)
-        {
-            if (buffer[j])
-            {
-                putchar(buffer[j]);
-            }
-        }
+		buffer[j] = value + '0';
+
+		for (j = sizeof(buffer) - 1; j >= 0; j--)
+		{
+			if (buffer[j])
+			{
+				_putchar(buffer[j]);
+			}
+		}
 
 		_putchar('\n');
 	}
