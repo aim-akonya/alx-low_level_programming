ea75a08d5fac3b95ade2456c22e7416f998cac6e
Mike <akonya@adtel.co.ke>
2023-02-28 13:18:14 +0300

    feat: more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,29 @@
 #include "main.h"
 
+/**
+* more_numbers - print numbers 1, 14, 10 times
+*/
+void more_numbers(void)
+{
+    int i;
 
+    for (i = 0; i < 10; i++)
+    {
+        int j;
+
+        for (j = 0; j < 15; j++)
+        {
+
+            if (j / 10)
+            {
+                _putchar(j/10);
+                _putchar(j%10);
+            } else
+            {
+                _putchar(j);
+            }
+        }
+        _putchar('\n');
+    }
+
+}
