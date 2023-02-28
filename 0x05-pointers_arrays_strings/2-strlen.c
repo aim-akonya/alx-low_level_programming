00aad7f05129de0a30a531bbfaf8c8ad174e14a2
michael <aim.akonya8@gmail.com>
2023-02-28 12:58:31 -0800

    feat: updates

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - print the size of a string
+* @s: string input
+* Return: length of input string
+*/
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		continue;
+	}
+	return (count);
+}
