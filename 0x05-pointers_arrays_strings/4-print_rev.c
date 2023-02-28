118f5988ab573cd96f8787e017ef1332117ff422
michael <aim.akonya8@gmail.com>
2023-02-28 13:18:08 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - reverses a string
+* @s: the input string
+*/
+void print_rev(char *s)
+{
+	int count, j;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		continue;
+	}
+
+	for (j = count - 1; j >= 0; j--)
+	{
+		putchar(s[j]);
+	}
+}
