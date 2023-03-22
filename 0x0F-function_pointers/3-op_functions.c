6ba10f94f0a78a8b04cd79fe7bcb09617eaa5f0a
Mike <akonya@adtel.co.ke>
2023-03-22 18:44:30 +0300

    feat: updates

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,23 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+* op_add - add two numbers
+* @a: first param
+* @b: second param
+* Return: returs the summation
+*/
 int op_add(int a, int b)
 {
     return (a + b);
 }
 
 
+/**
+* op_sub - add two numbers
+* @a: first param
+* @b: second param
+* Return: returs the subtraction
+*/
 int op_sub(int a, int b)
 {
     return (a - b);
 }
 
+/**
+* op_mul - add two numbers
+* @a: first param
+* @b: second param
+* Return: returs the multiplication
+*/
 int op_mul(int a, int b)
 {
     return (a * b);
 }
 
+/**
+* op_div - divide two numbers
+* @a: first param
+* @b: second param
+* Return: returs the division
+*/
 int op_div(int a, int b)
 {
     if (b == 0)
@@ -29,6 +52,12 @@ int op_div(int a, int b)
     return (a / b);
 }
 
+/**
+* op_mod - get the mod of a and b
+* @a: first param
+* @b: second param
+* Return: returns the mod
+*/
 int op_mod(int a, int b)
 {
     if (b == 0)
