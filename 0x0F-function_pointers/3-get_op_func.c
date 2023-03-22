1abada277d5f3d4242e4e4cfee23b3e6c9068edc
Mike <akonya@adtel.co.ke>
2023-03-22 18:49:33 +0300

    feat: updates

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,10 @@
 #include "c-calc.h"
 
-int (*get_op_func(char *s))(int, int)
+/**
+* get_op_func - This function returns a pointer to the function that corresponds to the operator given as a parameter
+* 
+*/
+int (*get_op_func(char *s)) (int a, int b)
 {
-    
+
 }
