ed495b300922cb3e8840c1a4eb57619ac7457c7e
Mike <akonya@adtel.co.ke>
2023-03-22 16:19:53 +0300

    feat: updates

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function that prints a name
+* @name: character array representing the name string
+* @f: function pointer
+* Return: does not return anything
+*/
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
