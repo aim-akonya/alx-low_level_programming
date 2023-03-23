fd7612e287a5ac3aec95cda7ec674f83d87f2936
Mike <akonya@adtel.co.ke>
2023-03-23 16:24:30 +0300

    feat: updates

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,20 @@
-#include "c-calc.h"
+#include "3-calc.h"
 
 /**
-* get_op_func - This function returns a pointer to the function that corresponds to the operator given as a parameter
-* 
+* get_op_func - This function returns a pointer to 
+* the function that corresponds to the operator given as a parameter
+*
+* @s: operator passed as argument to the program
+* Return: returns a pointer to the function that corresponds to the operator given
 */
 int (*get_op_func(char *s)) (int a, int b)
 {
-
+    //
+    //
+    //
+    //
+    //
+    //
 }
+
+
