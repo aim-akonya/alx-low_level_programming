92d29e3207158013f2a6b8abb4436c5afebc5f33
akonya <akonya@adtel.co.ke>
2023-03-23 22:13:49 +0300

    feat: updates

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
 * get_op_func - This function returns a pointer to 
@@ -7,14 +8,29 @@
 * @s: operator passed as argument to the program
 * Return: returns a pointer to the function that corresponds to the operator given
 */
-int (*get_op_func(char *s)) (int a, int b)
+int (*get_op_func(char *s)) (int , int)
 {
-    //
-    //
-    //
-    //
-    //
-    //
+    /* declare array of operations*/
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+
+    int i;
+
+    i = 0;
+    while (i < 6)
+    {
+        op_t op = ops[i];
+        if (op.op[0] == s)
+        {
+            return (op.f);
+        }
+    }
+
+    return (NULL);
 }
-
-
