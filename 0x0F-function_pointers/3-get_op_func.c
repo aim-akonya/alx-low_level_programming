c82a6d99e7a73ce08bc34f0318e22ddb6c321672
michael <aim.akonya8@gmail.com>
2023-03-23 12:23:11 -0700

    feat: update betty coding style

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,35 +2,37 @@
 #include <stdlib.h>
 
 /**
-* get_op_func - This function returns a pointer to 
+* get_op_func - This function returns a pointer to
 * the function that corresponds to the operator given as a parameter
 *
 * @s: operator passed as argument to the program
-* Return: returns a pointer to the function that corresponds to the operator given
+* Return: returns a pointer to the
+* function that corresponds to the operator given
 */
-int (*get_op_func(char *s)) (int , int)
+int (*get_op_func(char *s))(int, int)
 {
-    /* declare array of operations*/
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
 
-    int i;
+	int i;
 
-    i = 0;
-    while (i < 6)
-    {
-        op_t op = ops[i];
-        if (op.op[0] == s)
-        {
-            return (op.f);
-        }
-    }
+	i = 0;
 
-    return (NULL);
+	while (i < 6)
+	{
+		op_t op = ops[i];
+
+		if (op.op[0] == s)
+		{
+			return (op.f);
+		}
+	}
+
+	return (NULL);
 }
