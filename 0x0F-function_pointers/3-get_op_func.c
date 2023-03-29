48bcbc8e0c03fd9d354e0521be5573ca7cabdc5b
Mike <akonya@adtel.co.ke>
2023-03-29 13:35:51 +0300

    fun_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
 * get_op_func - This function returns a pointer to
@@ -12,6 +13,7 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
 	op_t ops[] = {
 	{"+", op_add},
 	{"-", op_sub},
@@ -20,18 +22,17 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-
-	int i;
-
+	
 	i = 0;
 
-	while (i < 6)
+	while (ops[i].op)
 	{
-		if (ops[i].op[0] == s)
+		if (ops[i].op[0] == *s)
 		{
 			return (ops[i].f);
 		}
+		i++;
 	}
-    
+	
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,34 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
 
 int main(int agrc, char *argv[])
 {
-    int val1;
-    int val2;
-    char ops;
+    int val1, val2, result;
+    char ops[2];
+    int (*op_func)(int, int);
+
+    if (agrc < 4) {
+        printf("Error\n");
+        exit(98);
+    }
+
+    val1 = atoi(argv[1]);
+    ops[0]= *argv[2];
+    val2 = atoi(argv[3]);
+
+    /*get the function pointer*/
+    op_func = get_op_func(ops);
+    
+    if (!op_func)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    /*execute the function pointe;*/
+    result = op_func(val1, val2);
+    printf("%d\n", result);
 
     return (0);
 }
