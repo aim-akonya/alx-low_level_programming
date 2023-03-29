d48d67e9d328e66d7ba46c86f92e13f2c47916b5
Mike <akonya@adtel.co.ke>
2023-03-29 15:31:27 +0300

    feat: updates

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,12 @@
+
+/**
+* main - main function
+* @argc: number of arguments
+* @argv: arrays of commandline arguments
+* Return: returns an int
+*/
+int main(int argc, char* argv[])
+{
+    
+    return (0);
+}
