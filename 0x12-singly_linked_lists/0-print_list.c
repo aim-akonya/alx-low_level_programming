0573e0879917b906dad1acbf1ff17f4214edf812
Mike <akonya@adtel.co.ke>
2023-03-30 13:20:52 +0300

    feat: updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,37 @@
 #include "lists.h"
 
+/**
+* print_list - prints all the elements of a list
+* @h: pointer to the head of the linked list
+* Return: returns the size of the linked list
+*/
 size_t print_list(const list_t *h)
 {
     list_t *node;
-    size_t nodec;
+    size_t node_c;
 
-    nodec = 0;
-    node = *h;
+    node_c = 0;
+    node = h->next;
+
+    if (h)
+    {
+        if(h->str)
+        {
+            printf("[%d] %s\n", h->len, h->str);
+        }
+        else
+        {
+            printf("[%d] (%s)\n", 0, "nil");
+        }
+        node_c++;
+        node = h->next;
+    }
+
+    while(node)
+    {
+        
+    }
 
 
-
-
-
-
-    return nodec;
+    return node_c;
 }
