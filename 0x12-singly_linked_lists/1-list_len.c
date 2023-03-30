2f077b4a723459478fedcfe5916e9c50f22ff5b5
Mike <akonya@adtel.co.ke>
2023-03-30 14:10:40 +0300

    feat: updates

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 
-
+/**
+* list_len: returns the number of nodes in a linked list
+* @h: head of a linked list
+* Return: returns the size of list
+*/
 size_t list_len(const list_t *h)
 {
     list_t *node;
