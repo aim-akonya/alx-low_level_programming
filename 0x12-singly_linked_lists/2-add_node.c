b0e2bd9d25585665a382359188e16b6623a15767
Mike <akonya@adtel.co.ke>
2023-03-30 15:27:34 +0300

    feat: updates

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,10 @@
 #include "lists.h"
+#include <>
 
 /**
 * add_node -- add a node at the head of a linked list
-* 
+* @head: pointer to the pointer to the head of the linked list
+* @str: string value of node
 */
 list_t *add_node(list_t **head, const char *str)
 {
