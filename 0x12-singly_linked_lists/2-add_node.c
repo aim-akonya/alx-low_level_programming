d0f59cce1582534490ca133e2bd92032eff2766b
Mike <akonya@adtel.co.ke>
2023-03-30 17:11:52 +0300

    feat: upds

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,12 +11,6 @@ list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
 
-
-    if (*head == NULL)
-    {
-        printf("reached\n");
-    }
-
     new_node = malloc(sizeof(list_t));
 
     if(new_node == NULL)
@@ -29,7 +23,7 @@ list_t *add_node(list_t **head, const char *str)
     new_node->len = strlen(str);
     new_node->next = *head;
 
-    head = &new_node;
+    *head = new_node;
 
     return (new_node);
 }
