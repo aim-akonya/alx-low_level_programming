acb57a2f1938fc621fdd76717342e8353b0fce81
michael <aim.akonya8@gmail.com>
2023-03-30 03:39:50 -0700

    feat: updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,40 +7,39 @@
 */
 size_t print_list(const list_t *h)
 {
-    list_t *node;
-    size_t node_c;
+	list_t *node;
+	size_t node_c;
 
-    node_c = 0;
-    node = h->next;
+	node_c = 0;
+	node = h->next;
 
-    if (h)
-    {
-        if(h->str)
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
-        else
-        {
-            printf("[%d] (%s)\n", 0, "nil");
-        }
-        node_c++;
-        node = h->next;
-    }
+	if (h)
+	{
+		if (h->str)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[%d] (%s)\n", 0, "nil");
+		}
+		node_c++;
+		node = h->next;
+	}
 
-    while(node)
-    {
-        if(node->str)
-        {
-            printf("[%d] %s\n", node->len, node->str);
-        }
-        else
-        {
-            printf("[%d] (%s)\n", 0, "nil");
-        }
-        node = node->next;
-        node_c++;
-    }
+	while (node)
+	{
+		if (node->str)
+		{
+			printf("[%d] %s\n", node->len, node->str);
+		}
+		else
+		{
+			printf("[%d] (%s)\n", 0, "nil");
+		}
+		node = node->next;
+		node_c++;
+	}
 
-
-    return node_c;
+	return (node_c);
 }
