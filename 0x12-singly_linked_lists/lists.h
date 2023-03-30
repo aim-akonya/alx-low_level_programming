9f78b6a4ae931cd733c8eacfe473842fa2de48c5
Mike <akonya@adtel.co.ke>
2023-03-30 12:19:03 +0300

    feat: updates

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,5 +19,6 @@ typedef struct list_s
 
 
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
