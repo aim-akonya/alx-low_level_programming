9530d23077fccd83f5f183f805dd99529c6cb7a0
Mike <akonya@adtel.co.ke>
2023-04-03 13:39:41 +0300

    feat: updates

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free list memory
+* @head: head of the list
+*/
+void free_listint2(listint_t **head)
+{
+    listint_t *val;
+
+	val = *head;
+	while (val)
+	{
+		listint_t *val1;
+
+		val1 = val;
+		val = val1->next;
+		free(val1);
+	}
+    *head = NULL;
+}
