45f73ffa90658ff108d1e1cedd160dd2d17f4d23
michael <aim.akonya8@gmail.com>
2023-04-03 04:27:36 -0700

    fix: update values

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,18 +7,18 @@
 */
 int pop_listint(listint_t **head)
 {
-    listint_t *new_head;
-    listint_t *curr_head;
-    int n;
+	listint_t *new_head;
+	listint_t *curr_head;
+	int n;
 
-    if(*head == NULL)
-    {
-        return (0);
-    }
-    curr_head = *head;
-    new_head = curr_head->next;
-    *head = new_head;
-    n = curr_head->n;
-    free(curr_head);
-    return (n);
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	curr_head = *head;
+	new_head = curr_head->next;
+	*head = new_head;
+	n = curr_head->n;
+	free(curr_head);
+	return (n);
 }
