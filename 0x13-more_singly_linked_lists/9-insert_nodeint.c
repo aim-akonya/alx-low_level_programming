0673d7810701ca11777b8285dcd0b43a4c66468d
michael <aim.akonya8@gmail.com>
2023-04-03 06:16:02 -0700

    fix: formatting

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,3 +1,4 @@
+
 #include "lists.h"
 
 /**
@@ -9,40 +10,39 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i;
-    listint_t *node;
-    listint_t *new_node;
+	unsigned int i;
+	listint_t *node;
+	listint_t *new_node;
 
-    i=0;
-    node = *head;
-    new_node = malloc(sizeof(listint_t));
-    new_node->n = n;
-    new_node->next = NULL;
-    
-
-    /*add to head*/
-    if (idx == 0)
-    {
-        new_node->next = node;
-        *head = new_node;
-        return (*head);
-    }
+	i = 0;
+	node = *head;
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	new_node->next = NULL;
 
 
-    while (node)
-    {
-        if (i == idx - 1)
-        {
-            listint_t *next_node;
+	/*add to head*/
+	if (idx == 0)
+	{
+		new_node->next = node;
+		*head = new_node;
+		return (*head);
+	}
 
-            next_node = node->next;
-            node->next = new_node;
-            new_node->next = next_node;
-            return (node->next);
-        }
-        node = node->next;
-        i++;
-    }
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			listint_t *next_node;
 
-    return (NULL);
+			next_node = node->next;
+			node->next = new_node;
+			new_node->next = next_node;
+			return (node->next);
+		}
+		node = node->next;
+		i++;
+	}
+
+	return (NULL);
 }
