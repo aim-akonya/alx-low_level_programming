847f2f1bf2dc90021e7bfe2e7056956479935127
Mike <akonya@adtel.co.ke>
2023-04-04 17:55:15 +0300

    feat: updates

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,6 +4,7 @@
 * _calloc: allocates memory for an array of nmemb elements of size size
 * @nmemb: members of the array
 * @size: size of the array
+* Return: returns a pointer to the block of memory
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -20,5 +21,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	return ptr;
+	return (ptr);
 }
