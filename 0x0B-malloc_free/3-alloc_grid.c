968cf60b2d6be48f849b7c24b63f135d56a88aa9
Mike <akonya@adtel.co.ke>
2023-04-04 15:37:15 +0300

    feat: updates

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* alloc_grid - allocate elemento to a 2d array
+* @width: width of the array
+* @height: heigh of the array
+* Return: returns a pointer to the array, NULL otherwise
+*/
+int **alloc_grid(int width, int height)
+{
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	
+}
