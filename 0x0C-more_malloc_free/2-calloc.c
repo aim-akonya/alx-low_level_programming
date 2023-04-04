08e61b76b048c64f68510870f2f148580ca45171
Mike <akonya@adtel.co.ke>
2023-04-04 17:52:45 +0300

    feat: updates

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _calloc: allocates memory for an array of nmemb elements of size size
+* @nmemb: members of the array
+* @size: size of the array
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	return ptr;
+}
