d4ee8842a7492705bfcd4408d3eff85ee2932ad4
Mike <akonya@adtel.co.ke>
2023-04-04 18:04:16 +0300

    feat: upds

--- 0x0C-more_malloc_free/2-main.c
+++ 0x0C-more_malloc_free/2-main.c
@@ -10,41 +10,45 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void simple_print_buffer(int *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *a;
+	int *a;
+	unsigned int nmemb;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Best");
-    strcpy(a + 4, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
-    return (0);
+	nmemb = 50;
+	a = _calloc(nmemb, sizeof(*a));
+	if (a == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	simple_print_buffer(a, nmemb);
+	free(a);
+	return (0);
 }
