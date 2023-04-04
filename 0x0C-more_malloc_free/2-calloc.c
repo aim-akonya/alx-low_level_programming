1df7ea3d14adde1b08427ac3aed7469bc3110859
Mike <akonya@adtel.co.ke>
2023-04-04 17:56:03 +0300

    feat: upds

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _calloc: allocates memory for an array of nmemb elements of size size
+* _calloc- allocates memory for an array of nmemb elements of size size
 * @nmemb: members of the array
 * @size: size of the array
 * Return: returns a pointer to the block of memory
