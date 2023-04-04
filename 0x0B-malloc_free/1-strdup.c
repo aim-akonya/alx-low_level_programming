d22d318d36e75648c4c92ca1b4fc1582bf5ace49
Mike <akonya@adtel.co.ke>
2023-04-04 12:01:27 +0300

    feat: updates

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* _strdup - return a copy of the input string
+* @str: input string
+* Return: pointer to the new char array
+*
+*/
+char *_strdup(char *str)
+{
+    char *str_cp;
+    unsigned int str_size;
+
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+
+
+    /*compute length of string*/
+    for (str_size = 0; str[str_size] != '\0'; ++str_size);
+
+    str_cp = malloc(str_size+1);
+    if (str_cp == NULL)
+    {
+        printf("reached %d\n", str_size);
+        return (NULL);
+    }
+
+    for (str_size = 0; str[str_size] != '\0'; ++str_size)
+    {
+        str_cp[str_size] = str[str_size];
+    }
+
+    str_cp[str_size] = '\0';
+
+    return str_cp;
+}
