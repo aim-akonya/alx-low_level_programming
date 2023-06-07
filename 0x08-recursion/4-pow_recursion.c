eb6f0a8bd7adc59adf869b7e2c2b7bdb1680f8f8
Mike <akonya@adtel.co.ke>
2023-06-07 17:49:25 +0300

    feat: updates

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,2 +1,33 @@
 #include "main.h"
 
+
+int pow_multiple(int x, int index, int result)
+{
+	if (index > 0)
+	{
+		
+		return (x);
+	}
+	return (result);
+}
+
+/**
+*  _pow_recursion - compute power of a number
+* @x: base value
+* @y: power value
+* Return: returns the power of the value
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+
+}
