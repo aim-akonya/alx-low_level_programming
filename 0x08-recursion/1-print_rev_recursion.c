f75a7ee437b8f9bfcb6271629033dfed0b658980
Mike <akonya@adtel.co.ke>
2023-06-07 13:14:37 +0300

    feat: upds

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,7 +10,7 @@ void print(char *s, int index)
 {
     if (index >= 0)
     {
-        if (s[index] != '\0')
+        if (s[index] == '\0')
         {
            print(s, --index); 
         } else {
