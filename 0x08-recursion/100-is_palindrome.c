4d43733ff0f4e01702c570c3be112cc6f74c2003
Mike <akonya@adtel.co.ke>
2023-06-08 13:15:14 +0300

    feat" upds

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,21 @@
 #include "main.h"
+#include "string.h"
+
+/**
+* compare_characters - compare string characters
+* @s: string value
+* @start: start index
+* @end: end index
+* @len: length of the string
+* Return: 1 if string if palindome, else 0
+*/
+int compare_characters(char *s, int start, int end, int len)
+{
+	if (start < len-1)
+	{
+
+	}
+}
 
 /**
 * is_palindrome - checks whether a string is a palindrome
@@ -7,5 +24,11 @@
 */
 int is_palindrome(char *s)
 {
-	
+	int start, end, len;
+
+	start = 0;
+	len = strlen(s);
+	end = len - 1;
+
+	return (compare_characters(s, start, end, len));
 }
