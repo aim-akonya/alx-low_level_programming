7897c5cc90028e1dad7ac5f882aec2c5b9f40c8d
Mike <akonya@adtel.co.ke>
2023-06-08 12:58:55 +0300

    feat: upds

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* is_palindrome - checks whether a string is a palindrome
+* @s: string parameter
+* Return: 1 if s is a palindrome, 0 otherwise
+*/
+int is_palindrome(char *s)
+{
+	
+}
