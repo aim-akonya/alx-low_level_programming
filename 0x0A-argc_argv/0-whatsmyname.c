6f33217d166386a488a43efb1b75c2ab579f51f1
Mike <akonya@adtel.co.ke>
2023-06-12 17:06:40 +0300

    feat: updates

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* main -  main function
+* @argc: arguments count
+* @argv: array of strings
+* Return: 0
+**/
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
