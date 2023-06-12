9bd0d54e64372b8d048701be502f2eea96fc4746
Mike <akonya@adtel.co.ke>
2023-06-12 17:12:15 +0300

    feat: upds

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main -  main function
+* @argc: arguments count
+* @argv: array of strings
+* Return: 0
+**/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
