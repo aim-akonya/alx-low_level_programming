77819b5277e9473643cd1f568a88c3dc411a0d46
Mike <akonya@adtel.co.ke>
2023-06-12 18:17:03 +0300

    feat: upds

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main -  main function
+* @argc: arguments count
+* @argv: array of strings
+* Return: 0
+**/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error");
+		exit (1);
+	}
+	return (0);
+}
