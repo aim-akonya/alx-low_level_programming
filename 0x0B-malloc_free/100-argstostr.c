eb1185c8c36d80295db5c89f71b9716f961b12c6
Mike <akonya@adtel.co.ke>
2023-06-13 13:41:29 +0300

    feat: updates

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,28 @@
 #include "main.h"
 #include "string.h"
 
+int count_args_len(int ac, char **av)
+{
+	int i, total_len;
+
+	total_len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		int str_pt;
+		char *val;
+
+		val = av[i];
+		str_pt = 0;
+		while (val[str_pt] != '\0')
+		{
+			str_pt++;
+			total_len++;
+		}
+	}
+
+	return (total_len);
+}
+
 /**
 * argstostr - concatenates all the arguments of your program
 * @ac: arguments count
