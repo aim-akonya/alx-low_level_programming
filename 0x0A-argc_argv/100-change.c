5b7187e71377c15450425530a62f775a687d6acd
Mike <akonya@adtel.co.ke>
2023-06-13 09:17:04 +0300

    feat: upds

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,10 +10,19 @@
 **/
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
+	int cents;
+
 	if (argc != 2)
 	{
 		printf("Error");
 		exit (1);
 	}
+
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("%d\n", 0);
+	}
+
 	return (0);
 }
