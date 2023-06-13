9ee2a1c250f8401905563074cf23de10a4b2d630
Mike <akonya@adtel.co.ke>
2023-06-13 12:40:45 +0300

    feat: upds

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,31 +5,24 @@
 /**
 * count - count min number of coins
 */
-int count(int coins[], int total_coins, int current_sum, int target_sum, int index, int iterations)
+int count(int coins[], int n, int sum)
 {
-	int i;
+	if (sum == 0)
+	{
+		return (1);
+	}
 
-	if (current_sum > target_sum)
+	if (sum < 0)
 	{
 		return (0);
 	}
 
-	if (current_sum == target_sum)
+	if (n <= 0)
 	{
-		printf ("Found target sum in %d iteration", iterations);
-		/**return (current_sum);**/
+		return 0;
 	}
 
-	for (i = 0; i < total_coins; i++)
-	{
-		int new_sum, iter, next;
-		new_sum = current_sum + coins[i];
-
-		iter = iterations +1;
-		next = index + 1;
-		return (count(coins, total_coins, new_sum, target_sum, next, iter));
-	}
-	return (0);
+	return count(coins, n - 1, sum) + count(coins, n, sum - coins[n - 1]);
 }
 
 /**
@@ -40,7 +33,7 @@ int count(int coins[], int total_coins, int current_sum, int target_sum, int ind
 **/
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	int cents;
+	int cents, total;
 	int coins[5] = {5, 2, 1, 25, 10};
 
 	if (argc != 2)
@@ -55,7 +48,7 @@ int main(int argc, char __attribute__((__unused__)) *argv[])
 		printf("%d\n", 0);
 	}
 
-	count(coins, 5, 0, cents, 0, 0);
-
+	total = count(coins, 5, cents);
+	printf("%d\n", total);
 	return (0);
 }
