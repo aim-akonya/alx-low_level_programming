94945a5332a184003aa6da695b080b7967a2f7be
akonya <aim.akonya8@gmail.com>
2023-06-13 13:15:43 -0700

    feat: upds

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,4 +8,23 @@
 char **strtow(char *str)
 {
 	char * name;
+	int i;
+	int start_idx, end_idx;
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	start_idx = -1;
+	end_idx = -1;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ')
+		{
+			continue;
+		}
+	}
+
 }
