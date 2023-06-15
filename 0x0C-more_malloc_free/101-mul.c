c38a198bfcab18a21b6b876ab13116b8483f5c7a
Mike <akonya@adtel.co.ke>
2023-06-15 17:34:37 +0300

    feat: updates

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+* str_to_int - converts a string to int
+* @str: string to be converted
+* Return: returns a string representation of the value
+*/
+int str_to_int(char * str)
+{
+	long val;
+	char *end_ptr;
+
+	val = strtol(str, &end_ptr, 10);
+	/* check if string cannot be converted*/
+	if (end_ptr == str)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/*check, number contains invalid characters */
+	if ('\0' != *end_ptr)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	return ((int) val);
+}
+
+/**
+* main - multiplies two positive numbers
+* @argc: number of arguments passed to the program
+* @argv: character arguments array
+*/
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	
+
+	return (0);
+}
