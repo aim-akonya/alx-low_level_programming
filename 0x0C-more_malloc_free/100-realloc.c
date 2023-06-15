a2baac48391ec5043e4fc28ed891fcfe2863c7b7
Mike <akonya@adtel.co.ke>
2023-06-15 14:25:36 +0300

    feat" updates

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _realloc - function that reallocates a memory block
+* @ptr: pointer to the previous memory allocated with a call to malloc
+* @old_size: the size, in bytes, of the allocated space for @ptr
+* @new_size: the size, in bytes, of the new memory block
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	
+}
