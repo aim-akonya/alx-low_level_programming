415ee5c121cb9366acf56f89033e4cfabc18bd33
Mike <akonya@adtel.co.ke>
2023-06-26 13:45:54 +0300

    print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct dog - Dog struct
@@ -17,5 +18,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 #endif
