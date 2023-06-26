559817a8921accce315d5805afc5c0fd67305553
Mike <akonya@adtel.co.ke>
2023-06-26 13:25:48 +0300

    feat: updates: dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog struct
+ *
+ * @name: name of the dog
+ * @age: Age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
