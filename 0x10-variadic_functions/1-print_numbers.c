9590eabf7784bcf27e1c77c1df5d09e661f310d2
Mike <akonya@adtel.co.ke>
2023-06-26 17:05:48 +0300

    feat: upds

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints numbers
+* @separator: separator for the numbers
+* @n: number of integers passed to the function
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	
+}
