f0cac23b2fa172a46d32d289ab0a8cfdc386c63f
Mike <akonya@adtel.co.ke>
2023-06-27 17:18:38 +0300

    feat: upds

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+
+/**
+* print_all - function that prints anything
+* @format: list of types of arguments passed to the function 
+* c: char, i: integer, f: float, s: char *
+* 
+*/
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	
+}
