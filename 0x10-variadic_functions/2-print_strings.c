140d7f19b7269a134ea6feeb00f7a0a340864958
Mike <akonya@adtel.co.ke>
2023-06-27 16:30:07 +0300

    feat: updates

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+* print_strings - print strings
+* 
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		char *str;
+		
+		str = va_arg(args, char*);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		
+		if (separator != NULL && i < n-1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
