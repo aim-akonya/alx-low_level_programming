8b076a53ba783a4c8449187cbb44a1ca41e5dbf2
Mike <akonya@adtel.co.ke>
2023-06-27 18:02:13 +0300

    feat: upds

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,7 +9,17 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
+	int idx;
 
 	va_start(args, format);
+	idx = 0;
+	while (format[idx] != '\0')
+	{
+		if (format[idx] != 'c' && format[idx] != 'i' && format[idx] != 'f' && format[idx] != 's')
+		{
+			continue;
+		}
+		
+	}
 	printf("\n");
 }
